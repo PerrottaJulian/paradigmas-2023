1848112dc2b65cdb5d40504b75f03f40bd1454c7
PerrottaJulian <111912746+PerrottaJulian@users.noreply.github.com>
2023-10-10 09:48:34 -0300

    10/10/2023

--- guia5/5-2.py
+++ guia5/5-2.py
@@ -1,86 +1,3 @@
-<<<<<<< Updated upstream:guia5/5-2.py
-## Sólo menores que 7. Desarrollar un programa de Phyton que permita cargar por teclado un secuencia de números, uno por uno. Siempre se supone que el usuario cargará un 0(cero) para indicar el final del proceso de carga. El cero no debe considerarse un dato a procesar. El programa debe:
-## a) Determinar el porcentaje que cantidad de números pares representa en la cantidad total de números ingresados.
-## b) Determinar cuántos de los números ingresados tenían su último dígito igual a 4 o igual a 5.
-## c) Determinar el menor de los números ingresados que sean divisibles por 3.
-## d) Determinar si la secuencia estaba formada sólo por números.
-
-
-#variables
-c_numeros = int();
-c_pares = int();
-iguales_4 = int();
-iguales_5 = int();
-menor_divisiblepor3 = int();
-
-
-c_numeros = 0;
-c_pares = 0;
-iguales_4 = 0;
-iguales_5 = 0;
-menor_divisiblepor3 = 999999;
-
-#modulos
-def esPar(numero):
-    if numero % 2 == 0:
-        return True
-    else:
-        return False
-
-def porcentaje (parte, total):
-    return parte*100/total
-
-def ultimoDigito(numero):
-    numero = str(numero);
-    ultimo_digito = numero[len(numero)-1];
-    ultimo_digito = int(ultimo_digito);
-    return ultimo_digito
-
-def IgualA(numero, numero_buscado):
-    if (numero == numero_buscado):
-        return True;
-    else:
-        return False;
-
-def divisiblePor(numero, divisor):
-    if numero % divisor == 0:
-        return True;
-    else:
-        return False;
-
-#codigo principal
-print("Debera ingresar una serie de numeros, para terminar la carga ingresar 0.")
-n = int(input("Ingrese un numero: "));
-
-while n != 0:
-    c_numeros += 1;
-
-    if esPar(n) == True:
-        c_pares += 1;
-
-    if IgualA(ultimoDigito(n), 4) == True:
-        iguales_4 += 1;
-    
-    if IgualA(ultimoDigito(n), 5) == True:
-        iguales_5 += 1;
-    
-    if divisiblePor(n, 3) == True:
-        if n < menor_divisiblepor3:
-            menor_divisiblepor3 = n;
-
-
-    n = int(input("Ingrese un numero: "));
-
-
-
-print("Porcentaje de numeros pares:", porcentaje(c_pares, c_numeros), "%");
-
-print("Numero con ultimo digito igual a 4:", iguales_4);
-print("Numero con ultimo digito igual a 5:", iguales_5);
-
-print("El menor de los numeros divisibles por 3 es:", menor_divisiblepor3);
-
-=======
 """
 Sólo menores que 7. Desarrollar un programa de Phyton que permita cargar por teclado un secuencia de números, uno por uno.
 Siempre se supone que el usuario cargará un 0(cero) para indicar el final del proceso de carga. El cero no debe considerarse un dato a procesar. El programa debe:
@@ -89,6 +6,7 @@ b) Determinar cuántos de los números ingresados tenían su último dígito igu
 c) Determinar el menor de los números ingresados que sean divisibles por 3.
 d) Determinar si la secuencia estaba formada sólo por números.
 """
+#2023
 
 #modulos
 def esPar(numero):
@@ -135,17 +53,25 @@ def principal(lista_numeros):
     solo_numeros = True
 
     for n in lista_numeros:
-        c_pares += esPar(n)
-        iguales_4 += IgualA(ultimoDigito(n),4)
-        iguales_5 += IgualA(ultimoDigito(n),5)
+        try:
+            n = int(n)
+            c_pares += esPar(n)
+            iguales_4 += IgualA(ultimoDigito(n),4)
+            iguales_5 += IgualA(ultimoDigito(n),5)
+            """if bandera:
+                menor_divisiblepor3 = n
+                bandera = False
+            elif divisiblePor(n, 3):
+                menor_divisiblepor3 = encontrarMenor(menor_divisiblepor3, n)"""
+            if (divisiblePor(n,3)):
+                if bandera:
+                    menor_divisiblepor3 = n
+                else:
+                    menor_divisiblepor3 = encontrarMenor(menor_divisiblepor3, n)
 
-        if bandera:
-            menor_divisiblepor3 = bandera = False
-        elif divisiblePor(n, 3):
-            menor_divisiblepor3 = encontrarMenor(menor_divisiblepor3, n)
-            
-        if str(n).isdigit() is False:
+        except:
             solo_numeros = False
+
 
     print(f"El porcentaje de numeros pares es: {porcentaje(c_pares, len(lista_numeros))}")
     print(f"Numeros con el ultimo digito igual a 4: {iguales_4}")
@@ -170,4 +96,4 @@ while n != "0":
        
 
 principal(numeros)
->>>>>>> Stashed changes:5-2.py
+
--- guia5/5-5.py
+++ guia5/5-5.py
@@ -1,6 +1,9 @@
-## Dada la fracción P/Q, para P y Q naturales informar la mayor cantidad de simplificaciones. Desarrolle y utilice una función que reciba dos números naturales y retorne el menor factor común. 
-## Ej: 360/60 = 180/30 = 90/15 = 30/5 = 6/1
-
+"""
+Dada la fracción P/Q, para P y Q naturales informar la mayor cantidad de simplificaciones. 
+Desarrolle y utilice una función que reciba dos números naturales y retorne el menor factor común. 
+Ej: 360/60 = 180/30 = 90/15 = 30/5 = 6/1 
+"""
+#2023
 
 #modulos
 def esDivisible(dividendo, divisor):
@@ -9,30 +12,35 @@ def esDivisible(dividendo, divisor):
     else:
         return False;
     
-def simplificar(num, deno):
-    fraccion = [num, deno]
+def simplificar(fraccion):
     divisores = [2,3,4,5,6,7,8,9];
     for divisor in divisores:
-        while (esDivisible(num, divisor) == True and esDivisible(deno, divisor) == True):
-            num //= divisor;
-            deno //= divisor;
-
-    return str(num) + "/" + str(deno)
-
+        if (esDivisible(fraccion[0], divisor) == True and esDivisible(fraccion[1], divisor) == True):
+            fraccion[0] //= divisor
+            fraccion[1] //= divisor
+            return fraccion
+    return 0
 #codigo principal
+P = -1
+Q = -1
+
+seguir = True
+
 print("Ingresar P/Q, siendo P y Q numeros naturales");
+while P <= 0:
+    try:
+        P = int(input("P = "));
+    except:
+        continue
 
-P = int(input("P = "));
-while P < 0:
-    print("Numero invalido.");
-    P = int(input("P = "));
+while Q <= 0:
+    try:
+        Q = int(input("Q = "));
+    except:
+        continue
 
+mifraccion = [P,Q]
 
-Q = int(input("Q = "));
-while Q < 0:
-    print("Numero invalido.");
-    Q = int(input("Q = "));
-
-
-print(simplificar(P, Q));
-    
+while mifraccion != 0:
+    print(f"{mifraccion[0]}/{mifraccion[1]} ", end="= ")
+    mifraccion = simplificar(mifraccion)
